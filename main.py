9bdd9c60870bf417ebe5ac932753f32583843899
Smart Ant Studio <hometikhon777@gmail.com>
2023-08-26 13:13:00 +0300

    1.1

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
-app = Flask(__name__, render_template='/')
+app = Flask(__name__, template_folder="Temp")
 
-@app.route("/")
+@app.route('/')
 def index():
-    render_template('index.html')
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
