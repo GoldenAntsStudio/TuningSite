d4f31c60eaefd57123dac3a533b1ec52648025d4
Smart Ant Studio <hometikhon777@gmail.com>
2023-09-09 11:57:44 +0300

    1.1

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 app = Flask(__name__, template_folder="Temp")
 
 facts = ['Факт1', 'Факт2', 'Факт3', 'Факт4']
+randomOR = ['Orel', 'Reshka']
+chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789'
 
 @app.route('/')
 def index():
@@ -12,6 +14,18 @@ def index():
 @app.route('/random-facts')
 def random_facts():
     return f'<h1>{random.choice(facts)}</h1> <a href="/">назад</a>'
+
+@app.route('/random')
+def random_OR():
+    return f'<h1>{random.choice(randomOR)}</h1> <a href="/">назад</a>'
+
+@app.route('/passgen')
+def passgen():
+    password = ''
+    for i in range(10):
+        password += str(random.choice(chars))
+
+    return f'<h1>{password}</h1> <a href="/">назад</a>'
 
 
 if __name__ == "__main__":
