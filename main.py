0aa4a4aff9dfe370c45be19e8ac1baa06cfda197
Smart Ant Studio <hometikhon777@gmail.com>
2023-09-09 11:45:35 +0300

    1.0

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
-
+import random
 app = Flask(__name__, template_folder="Temp")
+
+facts = ['Факт1', 'Факт2', 'Факт3', 'Факт4']
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/random-facts')
+def random_facts():
+    return f'<h1>{random.choice(facts)}</h1> <a href="/">назад</a>'
 
 
 if __name__ == "__main__":
